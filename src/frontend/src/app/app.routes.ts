edcdd26263f915b6c659e58cb03cb88de9934fbc
ozor <osortlan@gmail.com>
2025-02-25 19:19:49 +0100

    add home and login comps

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**',   redirectTo: '', pathMatch: 'full' }
+];
