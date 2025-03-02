f8bd6f2f2116f8b45403743857da99eebc021b23
ozor <osortlan@gmail.com>
2025-03-02 22:47:13 +0100

    added some dotnet unit tests

--- src/backend.tests/CreateStreamSessionHandlerTest.cs
+++ src/backend.tests/CreateStreamSessionHandlerTest.cs
@@ -6,28 +6,55 @@ using System.Linq;
 public class CreateStreamSessionHandlerTest
 {
     [Fact]
-    public void CreateStreamSessionCommand_should_enter_data_in_db()
+    public void CreateStreamSessionCommand_should_invoke_notification()
     {
         // setup
         var options = new DbContextOptionsBuilder<StreamSessionContext>()
-            .UseInMemoryDatabase(databaseName: "StreamSessionDatabase")
+            .UseInMemoryDatabase(databaseName: "StreamSessionDatabase1")
             .Options;
         using var mockDb = new StreamSessionContext(options);
         
         var mockNotificationService = new Mock<INotificationService>();
 
-        var request = new CreateStreamSessionRequest { title = "Test Session" };
+        var request = new CreateStreamSessionRequest("Test Session");
 
 
         // run
-        var handler = new CreateStreamSessionHandler(context, mockNotificationService.Object);
+        var handler = new CreateStreamSessionHandler(mockDb, mockNotificationService.Object);
         handler.Handle(request);
 
 
         // verify
         mockNotificationService.Verify(ns => ns.SendNotification(
-                "session-created",
-                "new stream session created: Test Session",
-                streamSession), Times.Once);
+            "session-created", 
+            "new stream session created: Test Session", 
+            It.IsAny<object>()), Times.Once);
+    }
+
+    [Fact]
+    public void CreateStreamSessionCommand_should_enter_data_in_db()
+    {
+        // setup
+        var options = new DbContextOptionsBuilder<StreamSessionContext>()
+            .UseInMemoryDatabase(databaseName: "StreamSessionDatabase2")
+            .Options;
+        using var mockDb = new StreamSessionContext(options);
+        
+        var mockNotificationService = new Mock<INotificationService>();
+
+        var request = new CreateStreamSessionRequest("Test Session");
+
+
+        // run
+        var handler = new CreateStreamSessionHandler(mockDb, mockNotificationService.Object);
+        handler.Handle(request);
+
+
+        // verify
+        var count = mockDb.StreamSessions.Count();
+        Assert.Equal(1, count);
+        var streamSession = mockDb.StreamSessions.SingleOrDefault();
+        Assert.NotNull(streamSession);
+        Assert.Equal("Test Session", streamSession.Title);
     }
 }
